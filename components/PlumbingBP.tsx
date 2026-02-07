866362229f63824e21729850fd144220acf21223
WebCore-host <webcore112@gmail.com>
2026-02-07 16:46:21 -0600

    Update PlumbingBP.tsx

--- components/PlumbingBP.tsx
+++ components/PlumbingBP.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PlumbingBg: React.FC<{ className?: string }> = ({ className = "" }) => {
-  return <img src="/photo/plumbing.jpg" alt="Texas Built Plumbing Background" className={className} />;
+  return <img src="/plumbing.jpg" alt="Texas Built Plumbing Background" className={className} />;
 };
 
 export default PlumbingBg;
